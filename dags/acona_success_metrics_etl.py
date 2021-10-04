30d5f26be1b1b78524c5044b244467e55c017b3c
Christian Ziegler <christian@acolono.com>
2021-10-04 15:18:48 +0200

    fix Matomo data pipeline.

--- dags/acona_success_metrics_etl.py
+++ dags/acona_success_metrics_etl.py
@@ -86,21 +86,21 @@ def acona_success_metrics_etl():
         """
         #### Transform task
         Prepare data structure.
-        {'date': '2021-10-01', 'matomo': {'https://www.acona.app/about': [], 'https://www.acona.app/metrics': [], 'https://www.acona.app/legal': [], 'https://www.acona.app/info': [], 'https://www.acona.app/': [], 'https://www.acolono.com/about': [], 'https://www.acolono/metrics': [], 'https://www.acolono.com/produkte': [], 'https://www.acolono.com/betterembed': [], 'https://www.acolono.com/': {'nb_uniq_visitors': 1, 'nb_users': 0, 'nb_visits': 1, 'nb_actions': 2, 'nb_visits_converted': 1, 'bounce_count': 0, 'sum_visit_length': 21, 'max_actions': 2, 'bounce_rate': '0%', 'nb_actions_per_visit': 2, 'avg_time_on_site': 21}, 'https://www.acolono.com/jobs': [], 'https://www.acolono.com/en/blog/drupal-kubernetes-getting-started-insights': []}, 'gsc': {}}        {'nb_uniq_visitors': 1, 'nb_users': 0, 'nb_visits': 1, 'nb_actions': 2, 'nb_visits_converted': 1, 'bounce_count': 0, 'sum_visit_length': 21, 'max_actions': 2, 'bounce_rate': '0%', 'nb_actions_per_visit': 2, 'avg_time_on_site': 21
-
-        #Require format:
-                {'metric_d_visits': {
+        #Required format:
+                {'metric_d_visits': [
                         {"url": "https://www.acolono.com", "value": "10", "date": "2021-09-19"},
                         {"url": "https://www.acolono.com/about", "value": "10", "date": "2021-09-19"},
                         {"url": "https://www.acolono.com/metrics", "value": "11", "date": "2021-09-19"}
-                        },
-                'metric_d_page_views': {
+                        ],
+                'metric_d_page_views': [
                         {"url": "https://www.acolono.com", "value": "10", "date": "2021-09-19"},
                         {"url": "https://www.acolono.com/about", "value": "10", "date": "2021-09-19"},
                         {"url": "https://www.acolono.com/metrics", "value": "11", "date": "2021-09-19"}
-                        }
+                        ]
                 }
         """
+
+        #metrics = {'date': '2021-10-01', 'matomo': {'https://www.acona.app/about': [], 'https://www.acona.app/metrics': [], 'https://www.acona.app/legal': [], 'https://www.acona.app/info': [], 'https://www.acona.app/': [], 'https://www.acolono.com/about': [], 'https://www.acolono/metrics': [], 'https://www.acolono.com/produkte': [], 'https://www.acolono.com/betterembed': [], 'https://www.acolono.com/': {'nb_uniq_visitors': 1, 'nb_users': 0, 'nb_visits': 1, 'nb_actions': 2, 'nb_visits_converted': 1, 'bounce_count': 0, 'sum_visit_length': 21, 'max_actions': 2, 'bounce_rate': '0%', 'nb_actions_per_visit': 2, 'avg_time_on_site': 21}, 'https://www.acolono.com/jobs': [], 'https://www.acolono.com/en/blog/drupal-kubernetes-getting-started-insights': []}, 'gsc': {}}
 
         datatables = {
           'nb_visits': 'metric_d_visits',
@@ -114,14 +114,23 @@ def acona_success_metrics_etl():
           'bounce_rate': 'metric_d_bounce_rate'
         }
 
+        # Create a dictionary for matomo data.
+        matomo_data = {}
+        # Inside the dictionary we want an array of values for every table.
+        for key in datatables:
+          matomo_data[datatables[key]] = []
+
         matomo = metrics.get('matomo')
         date = metrics.get('date')
-        matomo_data = {}
         for key in matomo:
           url = key
           values = matomo[key]
           for key in datatables:
-            matomo_data.extend{variable: {"url": url, "value": values[key], "date": date}}
+            if values:
+              values = values
+              matomo_data[datatables[key]].append({"url": url, "value": values[key], "date": date})
+            else:
+              matomo_data[datatables[key]].append({"url": url, "value": 0, "date": date})
 
         data = {'matomo': matomo_data}
         return data
@@ -138,14 +147,16 @@ def acona_success_metrics_etl():
 
         WAREHOUSE_TOKEN = Variable.get("WAREHOUSE_TOKEN")
         WAREHOUSE_URL = Variable.get("WAREHOUSE_URL")
-
-        matomo_data = metrics.get(matomo)
         command = ''
+
+        #START MATOMO
+        matomo_data = metrics.get('matomo')
         for key in matomo_data:
-          #data = {"url": "https://www.acolono.com", "value": "10", "date": "2021-09-19"}
+          #data = [{"url": "https://www.acolono.com", "value": "10", "date": "2021-09-19"}, {"url": "https://www.acolono.com", "value": "111", "date": "2021-09-20"}]
           table = key
-          data = matomo_data['key']
+          data = matomo_data[key]
           command = command + 'curl ' + WAREHOUSE_URL + '/' +  table + ' -X POST -H "Authorization: Bearer ' + WAREHOUSE_TOKEN +'" -H "Content-Type: application/json" -d \'' + json.dumps(data, ensure_ascii=False) + '\';'
+        #END MATOMO
         return command
 
     # [END load]
